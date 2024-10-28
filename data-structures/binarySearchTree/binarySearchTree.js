c45d02272b7a2bda8cefd193038717ecb479cd15
Jim124 <fengpingan065@gmail.com>
2024-10-28 20:27:15 +0000

    binary search tree

---
+++ data-structures/binarySearchTree/binarySearchTree.js
@@ -0,0 +1,77 @@
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+
+      while (true) {
+        if (value < currentNode.value) {
+          // left
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          //right
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          }
+          currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+  lookup(value) {
+    if (!this.root) {
+      return null;
+    }
+    let currentNode = this.root;
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return currentNode;
+      }
+    }
+    return null;
+  }
+  remove(value) {
+    if (!this.root) {
+      return false;
+    }
+    let currentNode = this.root;
+    let parentNode = null;
+    while (currentNode) {}
+  }
+}
+
+const tree = new BinarySearchTree(9);
+tree.insert(9);
+tree.insert(4);
+tree.insert(20);
+tree.insert(15);
+tree.insert(170);
+tree.insert(1);
+tree.insert(6);
+
+console.log(tree);
+const search = tree.lookup(15);
+console.log(search);
