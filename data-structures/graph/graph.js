f7c0405d1496b9e5487a2f38f20293ccc2a84b97
Jim124 <fengpingan065@gmail.com>
2024-11-03 12:38:36 +0000

    add graph data structure

---
+++ data-structures/graph/graph.js
@@ -0,0 +1,27 @@
+// graph type adjacency matrix, adjacency list, Edge List
+// types directed or Undirected Cyclic or Acyclic Weighted or Unweighted
+class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
+  addEdge(node1, node2) {
+    this.adjacentList[node1].push(node2);
+    this.adjacentList[node2].push(node1);
+    //undirected Graph
+  }
+}
+
+const myGraph = new Graph();
+myGraph.addVertex(0);
+myGraph.addVertex(1);
+myGraph.addVertex(2);
+myGraph.addVertex(3);
+myGraph.addVertex(4);
+myGraph.addVertex(5);
+myGraph.addVertex(6);
+myGraph.addEdge();
